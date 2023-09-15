67e2ca0025ebe3da70b7182ae4a5d4368ec66049
Vinicius Rodrigues <135380841+viniciusrodrigu@users.noreply.github.com>
2023-09-15 08:09:36 -0300

    versão 1.0

---
+++ click.js
@@ -0,0 +1,62 @@
+function yuri() {
+
+    const myJSON_filmes = ["Yuri Alberto", "18 de março de 2001", "Corinthians"];
+    console.log(myJSON_filmes);
+
+    document.getElementById("api").innerHTML =
+        '<hr>'+
+            "<h5>Jogador: " + myJSON_filmes[0] + "</h5>" + "<h5>Data de Nacimento: " + myJSON_filmes[1] + "</h5>" +
+                "<h5>time: " + myJSON_filmes[2] + "</h5>"
+        
+}
+
+function gil() {
+    const myJSON_filmes = ["Gil", "12 de junho de 1987 ", "Corinthians"];
+    console.log(myJSON_filmes);
+
+    document.getElementById("api").innerHTML =
+    '<hr>'+
+        "<h5>Jogador: " + myJSON_filmes[0] + "</h5>" + "<h5>Data de Nacimento: " + myJSON_filmes[1] + "</h5>" +
+        "<h5>time: " + myJSON_filmes[2] + "</h5>" 
+}
+
+function cassio() {
+    const myJSON_filmes = ["Cássio", "6 de junho de 1987", "Corinthians"];
+    console.log(myJSON_filmes);
+
+    document.getElementById("api").innerHTML =
+    '<hr>'+
+        "<h5>Jogador: " + myJSON_filmes[0] + "</h5>" + "<h5>Data de Nacimento: " + myJSON_filmes[1] + "</h5>" +
+        "<h5>time: " + myJSON_filmes[2] + "</h5>" 
+        }
+
+function fagner() {
+    const myJSON_filmes = ["Fagner","11 de junho de 1989", "Corinthians"];
+    console.log(myJSON_filmes);
+
+    document.getElementById("api").innerHTML =
+    '<hr>'+
+        "<h5> Jogador: " + myJSON_filmes[0] + "</h5>" + "<h5>Data de Nacimento: " + myJSON_filmes[1] + "</h5>" +
+        "<h5>time: " + myJSON_filmes[2] + "</h5>" 
+    }
+
+function renato() {
+    const myJSON_filmes = ["Renato Augusto", "8 de fevereiro de 1988", "Corinthians"];
+    console.log(myJSON_filmes);
+
+    document.getElementById("api").innerHTML =
+    '<hr>'+
+    "<h5>Jogador: " + myJSON_filmes[0] + "</h5>" +"<h5>Data de Nacimento: " + myJSON_filmes[1] + "</h5>" +
+        "<h5>time: " + myJSON_filmes[2] + "</h5>"
+    }
+
+function fausto() {
+    const myJSON_filmes = ["Fausto Vera", "26 de março de 2000", "Corinthians"];
+    console.log(myJSON_filmes);
+
+    document.getElementById("api").innerHTML =
+         '<hr>'+
+        "<h5>Jogador: " + myJSON_filmes[0] + "</h5>" + "<h5>Data de Nacimento: " + myJSON_filmes[1] + "</h5>" +
+        "<h5>time: " + myJSON_filmes[2] + "</h5>"
+
+    }
